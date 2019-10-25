be7dc0af00558516da3808fad2c72fbd609780f0
Trewin Copplestone <tcopplestone@gmail.com>
2019-10-25 09:37:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -22,11 +22,10 @@ def map_to_double(source_array)
   new_array = []
   counter = 0
   while counter < source_array.length do
-    new_array.push(source_array[counter])
+    new_array.push((source_array[counter])*2)
     counter += 1
   end
   return new_array
-
 end
 
 def map_to_square(source_array)
