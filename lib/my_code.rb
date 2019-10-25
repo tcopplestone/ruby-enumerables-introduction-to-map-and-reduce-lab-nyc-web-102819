da6dc48fb045e23e91e6091a52caf34f995156cc
Trewin Copplestone <tcopplestone@gmail.com>
2019-10-25 09:38:16 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -29,7 +29,13 @@ def map_to_double(source_array)
 end
 
 def map_to_square(source_array)
-
+  new_array = []
+  counter = 0
+  while counter < source_array.length do
+    new_array.push((source_array[counter])**2)
+    counter += 1
+  end
+  return new_array
 end
 
 def reduce_to_total(source_array, starting_point)
