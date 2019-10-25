fe6005a3d7c014f5ca080b9ab15385ed209d1edd
Trewin Copplestone <tcopplestone@gmail.com>
2019-10-25 09:55:26 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -49,8 +49,9 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
-  teller = 0
-  
+  counter = 0
+  while counter < source_array.length do
+    
 
 end
 
