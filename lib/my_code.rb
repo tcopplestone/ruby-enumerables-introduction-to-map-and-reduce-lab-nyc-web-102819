48d8fb1773f8dcf8db7f41a21317fc7f5d188567
Trewin Copplestone <tcopplestone@gmail.com>
2019-10-25 09:41:31 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,7 +39,13 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point)
-  
+  total = starting_point
+  counter = 0
+  while counter < source_array.length do
+    total = total + source_array[counter]
+    counter += 1
+  end
+  return total
 end
 
 def reduce_to_all_true(source_array)
