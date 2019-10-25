9ddb77ce400152b587ae193d07e2fa913698b9ec
Trewin Copplestone <tcopplestone@gmail.com>
2019-10-25 09:31:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,7 +2,7 @@ def map_to_negativize(source_array)
   new_array = []
   counter = 0
   while counter < source_array.length do
-    new_array.
+    new_array.push()
 end
 
 def map_to_no_change(source_array)
