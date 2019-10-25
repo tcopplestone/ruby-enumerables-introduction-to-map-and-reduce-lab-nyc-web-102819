2c1d672bcdf6b0bd3c43fcb3cd068089b004b785
Trewin Copplestone <tcopplestone@gmail.com>
2019-10-25 09:58:32 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -53,8 +53,10 @@ def reduce_to_all_true(source_array)
   while counter < source_array.length do
     if source_array[counter] == false
       return false
-    
-
+    end
+    counter += 1
+  end
+  return true
 end
 
 def reduce_to_any_true(source_array)
